d672c328ca2bff3ea823d7ca8b66e0def2e3b116
dqnk <erik.kmetic@gmail.com>
2021-11-01 11:43:23 +0100

    cursor in wayland

--- fish/.config/fish/conf.d/config.fish
+++ fish/.config/fish/conf.d/config.fish
@@ -11,6 +11,7 @@ fish_vi_key_bindings
 export QT_QPA_PLATFORMTHEME=qt5ct
 export EDITOR="nvim"
 export PATH="/opt/cuda/bin:$PATH"
+export WLR_NO_HARDWARE_CURSORS=1
 
 set -g theme_display_git_default_branch yes
 set -g theme_color_scheme nord
