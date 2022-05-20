a394933aa8433d54d8eeedcbeb0f45a3722be560
dqnk <erik.kmetic@gmail.com>
2022-05-20 20:19:12 +0200

    new SpaceVim config for competitive programming, algs and data structs

---
+++ SpaceVim/.SpaceVim.d/skeletons/cp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define MOD 1000000007
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long long ll;
+
+void solve() {}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+  freopen("!input.txt", "r", stdin);
+#endif
+
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
