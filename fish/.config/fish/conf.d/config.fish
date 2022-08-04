5a19ec2c19328a4f5a0b8c76338c67216cd12d76
dqnk <64268180+dqnk@users.noreply.github.com>
2022-08-04 12:01:22 +0200

    fish conifg better

--- fish/.config/fish/conf.d/config.fish
+++ fish/.config/fish/conf.d/config.fish
@@ -7,18 +7,17 @@ alias sudo='doas'
 alias yay='paru'
 fish_vi_key_bindings
 
-set -Ux BUN_INSTALL "/home/dqnk/.bun"
 fish_add_path "/home/dqnk/.bun/bin"
-
-
-export QT_QPA_PLATFORMTHEME="qt5ct"
-export EDITOR="nvim"
-export VISUAL="nvim"
 fish_add_path /opt/cuda/bin
 fish_add_path ~/.local/bin/
-export WLR_NO_HARDWARE_CURSORS=1
-export NODE_OPTIONS=--max_old_space_size=8192
 
+set QT_QPA_PLATFORMTHEME qt5ct
+set EDITOR nvim
+set VISUAL nvim
+set -Ux BUN_INSTALL "/home/dqnk/.bun"
+set WLR_NO_HARDWARE_CURSORS 1
+set NODE_OPTIONS=--max_old_space_size 8192
+set SSH_AUTH_SOCK /run/user/1000/keyring/ssh
 set -g theme_display_git_default_branch yes
 set -g theme_color_scheme nord
 
