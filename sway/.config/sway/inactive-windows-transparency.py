abbcb79d922a83c5905b132dea0824fdfd3b3356
dqnk <64268180+dqnk@users.noreply.github.com>
2022-09-11 11:06:26 +0200

    inactive window transparency

--- sway/.config/sway/inactive-windows-transparency.py
+++ sway/.config/sway/inactive-windows-transparency.py
@@ -4,17 +4,19 @@ ipc = i3ipc.Connection()
 prev_focused = None
 
 for window in ipc.get_tree():
-	if window.focused:
-		prev_focused = window
-	else:
-		window.command('opacity 0.8')
+    if window.focused:
+        prev_focused = window
+    else:
+        window.command('opacity 0.95')
+
 
 def on_window_focus(ipc, focused):
-	global prev_focused
-	if focused.container.id != prev_focused.id: # https://github.com/swaywm/sway/issues/2859
-		focused.container.command('opacity 1')
-		prev_focused.command('opacity 0.925')
-		prev_focused = focused.container
+    global prev_focused
+    if focused.container.id != prev_focused.id:  # https://github.com/swaywm/sway/issues/2859
+        focused.container.command('opacity 1')
+        prev_focused.command('opacity 0.95')
+        prev_focused = focused.container
+
 
 ipc.on("window::focus", on_window_focus)
 ipc.main()
