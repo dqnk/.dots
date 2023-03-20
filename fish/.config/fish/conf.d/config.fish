e5d8de80118a268e1935297e188c02d6c3df0675
dqnk <64268180+dqnk@users.noreply.github.com>
2023-03-20 15:09:47 +0100

    rust path

--- fish/.config/fish/conf.d/config.fish
+++ fish/.config/fish/conf.d/config.fish
@@ -8,16 +8,15 @@ alias yay='paru'
 alias dub="docker compose up --build"
 fish_vi_key_bindings
 
-fish_add_path "/home/dqnk/.bun/bin"
 
 
 fish_add_path /opt/cuda/bin
 fish_add_path ~/.local/bin/
+fish_add_path ~/.cargo/bin/
 
 set QT_QPA_PLATFORMTHEME qt5ct
 set EDITOR nvim
 set VISUAL nvim
-set -Ux BUN_INSTALL "/home/dqnk/.bun"
 set WLR_NO_HARDWARE_CURSORS 1
 set NODE_OPTIONS --max_old_space_size=8192
 export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
