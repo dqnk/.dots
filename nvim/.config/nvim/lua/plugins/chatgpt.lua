5a60efaf3fe2b9dc8bd98fcb5d9d3887f73d2e0b
dqnk <64268180+dqnk@users.noreply.github.com>
2023-09-03 11:19:00 +0200

    chatgpt, sniprun

--- nvim/.config/nvim/lua/plugins/chatgpt.lua
+++ nvim/.config/nvim/lua/plugins/chatgpt.lua
@@ -2,6 +2,13 @@ return {
   {
     "jackMort/ChatGPT.nvim",
 
+    keys = {
+
+      { "<leader>as", "<cmd>ChatGPT<cr>", desc = "ChatGPT" },
+      { "<leader>af", "<cmd>ChatGPTActAs<cr>", desc = "ChatGPT act as" },
+      { "<leader>ac", "<cmd>ChatGPTCompleteCode<cr>", desc = "ChatGPT complete code" },
+      { "<leader>an", "<cmd>ChatGPTEditWithInstructions<cr>", desc = "ChatGPT edit with instructions" },
+    },
     config = function()
       require("chatgpt").setup({ api_key_cmd = "gpg --decrypt " .. os.getenv("HOME") .. "/.openai-secret.txt.gpg" })
     end,
