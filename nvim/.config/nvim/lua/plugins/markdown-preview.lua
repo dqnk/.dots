ad05688c114c8caabbc3f5241ff1febe2f05f808
dqnk <64268180+dqnk@users.noreply.github.com>
2023-09-10 20:11:33 +0200

    markdown preview

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,6 +1,14 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
+    keys = {
+      {
+        "<localleader>ll",
+        "<cmd>MarkdownPreview<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    ft = "markdown",
     config = function()
       vim.fn["mkdp#util#install"]()
     end,
