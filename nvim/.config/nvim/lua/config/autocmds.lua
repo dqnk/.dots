67186d423f241b269f211632e42ab270c93de298
dqnk <64268180+dqnk@users.noreply.github.com>
2023-10-06 17:51:32 +0200

    merge

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.cmd([[autocmd BufEnter * set wrap]])
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = "*.tex",
+  command = "0r ~/.config/nvim/skeletons/template.tex",
+})
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+  pattern = "*.cpp",
+  command = "0r ~/.config/nvim/skeletons/template.cpp",
+})
