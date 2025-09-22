c76042186ba165bceb9c0d5b60b676a8b5a45bb9
dqnk <64268180+dqnk@users.noreply.github.com>
2025-09-22 09:21:47 +0200

    undotree removed

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- add symbols-outline
-  {
-    "mbbill/undotree",
-    cmd = "Undotree",
-    keys = { { "<F7>", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" } },
-    config = function() end,
-  },
-}
