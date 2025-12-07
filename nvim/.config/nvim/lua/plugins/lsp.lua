bd7bb1bd1ab4f4ed0b0ccaee4c2fa68e7d0db546
dqnk <64268180+dqnk@users.noreply.github.com>
2025-12-07 16:29:23 +0100

    racket lsp, waybar

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,5 +2,11 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = true },
+    servers = {
+      racket_langserver = {
+        cmd = { "racket", "--lib", "racket-langserver", "--", "--stdio" },
+        filetypes = { "racket", "scheme" },
+      },
+    },
   },
 }
