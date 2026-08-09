230600a8f482bec46ecbdd4816cff8ee354c399c
dqnk <64268180+dqnk@users.noreply.github.com>
2026-08-09 17:37:47 +0200

    fisher

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,13 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+    # bootstrap fisher and plugins listed in fish_plugins on first run
+    if not functions -q fisher
+        curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+        fisher update
+    end
+
+    # must run after fzf.fish installs its default bindings from conf.d
+    if functions -q fzf_configure_bindings
+        fzf_configure_bindings --directory=\cf --variable= --git_status= --history=
+        set -g fzf_fd_opts --hidden --max-depth 5
+    end
 end
